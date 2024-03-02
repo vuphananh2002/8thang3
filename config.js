4388ffe2cb03b7f8b27ccbd310280e1763dc6ff7
vuphananh2002 <161978916+vuphananh2002@users.noreply.github.com>
2024-03-03 04:29:47 +0700

    Add files via upload
    thiepmung

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'bé iu';
+const giftUrl = 'https://www.facebook.com/thanhthulas1tg';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'tặng vợ của anh';
+const contentCard = 'Chúc vợ  của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và đến mãi sau này. cảm ơn em đã đến bên anh ! Anh yêu em rất nhiều <3 NHÌN RA CỬA NÈ !';
+
+
+const giftImage = 'voiu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
